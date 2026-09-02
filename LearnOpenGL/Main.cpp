f0b6257d6c3b0f2204e89d74b33d7b9e6570d30b
Anıl Emre Akkoç <emreanil1224@gmail.com>
2026-09-02 20:02:12 +0300

    triangle(finally)

--- LearnOpenGL/Main.cpp
+++ LearnOpenGL/Main.cpp
@@ -2,6 +2,24 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+//Vertex Shader source code
+const char* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"\n"
+"void main()\n"
+"{\n"
+"    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\n";
+
+//Fragment Shader source code
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"\n"
+"void main()\n"
+"{\n"
+"    FragColor = vec4(0.8f, 0.3f, 0.2f, 1.0f);\n"
+"}\n";
+
 int main()
 {
 	// Initialize GLFW
@@ -14,6 +32,14 @@ int main()
 	// Tell GLFW we are using the CORE profile
 	// So that means we only have the modern functions
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// Vertices coordinates
+	GLfloat vertices[] =
+	{
+		-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, //lower left corner
+		0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,	//lower right corner
+		0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f // Upper corner
+	};
 
 	//Create a GLFWWindow object 800 by 800 pixels, naming it "YoutubeOpenGL"
 	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
@@ -34,7 +60,48 @@ int main()
 	// In this case the viewport goes from x = 0, y = 0, to x = 800, y = 800;
 	glViewport(0, 0, 800, 800);
 
-	// Specify the color color of the background
+	//Create Vertex Shader and gets its referance
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	//Attach Vertex Shader source tothe Vertex Shader Object
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	//Compile the Vertex Shader into machine code
+	glCompileShader(vertexShader);
+
+	//Create Fragment Shader and gets its referance
+	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	//Attach Fragment Shader source to the Fragment Shader Object
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	//Compile the Vertex Shader into machine code
+	glCompileShader(fragmentShader);
+
+	//Create Shadow Program
+	GLuint shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	// Wrap-up/Link all the shaders together into the Shader Program
+	glLinkProgram(shaderProgram);
+
+	//Delete useless Vertex and Fragment Shader Program
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	GLuint VAO, VBO;
+
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+
+	//// Specify the color color of the background
 	glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
 	// Clean the back buffer and assing the new color to it
 	glClear(GL_COLOR_BUFFER_BIT);
@@ -44,9 +111,21 @@ int main()
 	// Main while loop
 	while (!glfwWindowShouldClose(window))
 	{
+		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glfwSwapBuffers(window);
+
 		// Take care of all GLFW events
 		glfwPollEvents();
 	}
+
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteProgram(shaderProgram);
+
 	// Delete window before ending the program
 	glfwDestroyWindow(window);
 	// Terminate GLFW before ending the program
