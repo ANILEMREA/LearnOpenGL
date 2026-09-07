5197be327c5391d1641e3772aa51036c4192c89c
Anıl Emre Akkoç <emreanil1224@gmail.com>
2026-09-07 16:36:22 +0300

    EBO

--- LearnOpenGL/Main.cpp
+++ LearnOpenGL/Main.cpp
@@ -38,7 +38,18 @@ int main()
 	{
 		-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, //lower left corner
 		0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,	//lower right corner
-		0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f // Upper corner
+		0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f, // Upper corner
+		//Inner triangle
+		-0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f, //Inner left
+		0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f, //Inner right
+		0.0f, -0.5f * float(sqrt(3)) / 3, 0.0f//Inner down
+	};
+
+	GLuint indices[] =
+	{
+		0, 3, 5, // lower left triangle
+		3, 2, 4, // lower right triangle
+		5, 4, 1  // upper triangle
 	};
 
 	//Create a GLFWWindow object 800 by 800 pixels, naming it "YoutubeOpenGL"
@@ -85,37 +96,41 @@ int main()
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
 
-	GLuint VAO, VBO;
+	GLuint VAO, VBO, EBO;
 
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
 
 	glBindVertexArray(VAO);
-
+	//VBO
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+	//EBO(Index Buffer)
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	//VertexAtribArray
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
+	// Bind both the VBO and VAO to 0 so thast we don't accidentally modify the VAO 
+	// and VBO we created
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
-
-	//// Specify the color color of the background
-	glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
-	// Clean the back buffer and assing the new color to it
-	glClear(GL_COLOR_BUFFER_BIT);
-	// Swap the back buffer with the the front buffer
-	glfwSwapBuffers(window);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
 	// Main while loop
 	while (!glfwWindowShouldClose(window))
 	{
+		//Background
 		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
 		glfwSwapBuffers(window);
 
 		// Take care of all GLFW events
@@ -124,6 +139,7 @@ int main()
 
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
 	glDeleteProgram(shaderProgram);
 
 	// Delete window before ending the program
